98c1d932f9010ece5564f739a8f36e3ff72779c3
alan <alan@compendiumdev.co.uk>
2022-05-24 20:02:52 +0100

    adding tests for markdown conversion from text to data grid

--- data_formats/markdown_handler.js
+++ data_formats/markdown_handler.js
@@ -6,11 +6,27 @@ export class MarkdownConvertor {
 
     // todo: create a GenericDataTable class with a 'headers' array and a rowdata[][] array
     markdownTableToDataRows(markdownTable){
-        const rows = markdownTable.split(/[\r\n]+/);
-        var data = [];
-        var rowCount = 0;
-        rows.forEach(row =>{
+
+        // todo: split is removing blank lines in the middle rather than stopping processing early
+        let rows = markdownTable.split(/[\r\n]+/);
+        let data = [];
+        let rowCount = 0;
+        let processingStarted = false;
+        for(const row of rows){
           var rowString = row.trim();
+
+          if(processingStarted===true && rowString.length===0){
+              // skip empty it is a gap in the table
+              break;
+          }
+
+          if(processingStarted===false && rowString.length===0){
+            // skip empty lines
+            continue;
+          }else{
+            processingStarted=true;
+          }
+          
           
           if(rowString.charAt(0)=="|"){
             rowString = rowString.substring(1);
@@ -24,12 +40,12 @@ export class MarkdownConvertor {
           if(rowString.length>0 && rowCount!=1){
             var values = rowString.split("|");
             var cellValues = values.map(contents => contents.trim());
-            console.log(cellValues);
+            //console.log(cellValues);
             data.push(cellValues);
           }
     
           rowCount++;      
-        });
+        };
     
         return data;
     }

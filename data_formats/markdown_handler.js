3faaee9fb6674db4304a1946aaec5b88b444d21c
alan <alan@compendiumdev.co.uk>
2022-05-26 00:39:05 +0100

    validation of markdown table separators

--- data_formats/markdown_handler.js
+++ data_formats/markdown_handler.js
@@ -1,9 +1,64 @@
 export class MarkdownConvertor {
 
+  
     // todo add any configuration options into the markdown converter
-    constructor() {
+    constructor(params) {
+      this.validateSeperatorLength=false;
+      
+      if(params!==undefined){
+        if(params.hasOwnProperty("validateSeperatorLength")){
+          this.validateSeperatorLength = params.validateSeperatorLength;
+        }
+      }
     }
 
+    isMarkdownTableSeperatorRowValid(theRow){
+
+      let rowString = theRow.trim();
+      let values = rowString.split("|");
+
+      if(values[0]!=""){
+        return false; // first value should be empty
+      }
+
+      if(values[values.length-1]!=""){
+        return false; // last value should be empty
+      }
+
+      values = values.slice(1,-1); // remove first and last items which are empty
+
+      let cellValues = values.map(contents => {return contents.trim()});
+
+      let sizeCheckedValues = cellValues.filter(value => {
+
+        if(value.startsWith(":")){
+          value = value.slice(1); // remove first char
+        }
+        if(value.endsWith(":")){
+          value = value.slice(0,-1); // trim last char
+        }
+
+        let nominus = value.replace(/-/g,"");
+        // nominus should now be empty
+        if(nominus.length!=0){
+          // not valid
+          return false;
+        }else{
+          if(value.length<3){
+            // not valid
+            return false;
+          }
+        }
+        return true;
+      })
+
+      if(sizeCheckedValues.length<cellValues.length){
+        return false;
+      }
+
+      return true;
+    }
+    
     // todo: create a GenericDataTable class with a 'headers' array and a rowdata[][] array
     markdownTableToDataRows(markdownTable){
 
@@ -13,7 +68,7 @@ export class MarkdownConvertor {
         let rowCount = 0;
         let processingStarted = false;
         for(const row of rows){
-          var rowString = row.trim();
+          let rowString = row.trim();
 
           if(processingStarted===true && rowString.length===0){
               // skip empty it is a gap in the table
@@ -35,8 +90,18 @@ export class MarkdownConvertor {
             rowString=rowString.slice(0, -1);
           }
     
-          var rowString = rowString.trim();
+          rowString = rowString.trim();
     
+          // it is the "|---|" separator row
+          if(rowString.length>0 && rowCount==1){
+            if(this.validateSeperatorLength){
+              if(!this.isMarkdownTableSeperatorRowValid(rowString)){
+                // not valid return empty data
+                return [];
+              }
+            }
+          }
+
           if(rowString.length>0 && rowCount!=1){
             var values = rowString.split("|");
             var cellValues = values.map(contents => contents.trim());
--- tests/data_formats/markdown_handler.test.js
+++ tests/data_formats/markdown_handler.test.js
@@ -1,5 +1,52 @@
 import {MarkdownConvertor} from '../../data_formats/markdown_handler';
 
+describe("a valid header is a minimum of |---|---| but we also support :", ()=>{
+
+    test('when only - it fails', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("|-|-|")).toBe(false);
+    });
+
+    test('when only -- it fails', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("|--|--|")).toBe(false);
+    });
+
+    test('when empty it fails', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("|  |  |")).toBe(false);
+    });
+
+    test('when very empty it fails', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("|||")).toBe(false);
+    });
+
+    test('when only - single column', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("|--|")).toBe(false);
+    });
+
+    test('when ---  it passes', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("|---|")).toBe(true);
+    });
+
+    test('when mix of valid  it passes', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("|---|----|-----|------|")).toBe(true);
+    });
+
+    test('in theory there is npt a limit to length', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("|---|-----------------------------------------------------|-----|------|")).toBe(true);
+    });
+
+    test('preceding and trailing spaces are ignored', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("   | --- | ----|----- | ------|     ")).toBe(true);
+    });
+
+    test('alignment values are allowed', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("   | :---        |    :----:   |          ---: |     ")).toBe(true);
+    });
+
+    test('alignment values are validated', () => {
+        expect(new MarkdownConvertor().isMarkdownTableSeperatorRowValid("   | ::---        |    :----:   |          ---: |     ")).toBe(false);
+    });
+});
+
 describe("Can convert markdown tables to data suitable for a data grid",()=>{
 
     test('can convert a simple 2x3 table', () => {
@@ -109,5 +156,21 @@ describe("Can convert markdown tables to data suitable for a data grid",()=>{
         expect(data[0][1]).toBe('heading -2');
         expect(data[1][0]).toBe('row 0 cell 0');
         expect(data[1][1]).toBe('row 0 cell 1');
+    });
+
+
+    test('table should have 3 or more - 1 is not valid', () => {
+        const basicTable =
+`|heading -1|heading -2|
+|-|-|
+|row 0 cell 0|row 0 cell 1|
+`    
+        let data = new MarkdownConvertor({validateSeperatorLength:true}).markdownTableToDataRows(basicTable);
+
+        //todo: convert data to a GenericDataTable here and use that in all our tests, then migrate the code to use GenericDataTable
+        //console.log(data);
+
+        console.log(data);
+        expect(data.length).toBe(0);
     });
 });

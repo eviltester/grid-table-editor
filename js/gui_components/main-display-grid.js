5eeeeda5f2cedc8ccfa71429268b6534086d91df
alan <alan@compendiumdev.co.uk>
2022-05-31 10:34:46 +0100

    started creating a main grid component to isolate GUI functionality better

---
+++ js/gui_components/main-display-grid.js
@@ -0,0 +1,66 @@
+import { GridExtension } from "../grid/gridExtension.js";
+import { GridControl, GridControlsPageMap } from "./gridControl.js"
+import { CustomHeader } from "../grid/customHeader.js";
+
+class ExtendedDataGrid {
+
+    constructor() {
+        var rowData = [];
+
+        var columnDefs = [
+            {
+                headerName: '~rename-me',
+                field: 'column1'
+            }
+        ];
+
+        this.gridOptions = {
+            columnDefs: columnDefs,
+            rowData: rowData,
+
+            defaultColDef: {
+                wrapText: true,
+                autoHeight: true,
+                resizable: true,
+                rowDrag: true,
+                editable: true,
+                filter:true,
+                sortable:true,
+            },
+
+            components: {
+                agColumnHeader: CustomHeader,
+            },
+            
+            rowDragManaged: true,
+            rowDragMultiRow: true,
+            rowSelection: 'multiple',
+            //onColumnResized: (params) => {params.api.resetRowHeights();}
+        };
+    }
+
+    createChildGrid(parentGridDiv){
+
+        let gridDiv = parentGridDiv;
+
+        new agGrid.Grid(gridDiv, this.gridOptions);
+
+        // having setup the grid, make it a little more responsive
+        setTimeout(function(gridDiv){gridDiv.style.height="40%";},1000, gridDiv);
+
+        this.gridExtras = new GridExtension(this.gridOptions.api, this.gridOptions.columnApi);
+
+        let gridControls = new GridControl(this.gridExtras, new GridControlsPageMap());
+        gridControls.addHooksToPage(document);
+    }
+
+    getGridApi(){
+        return this.gridOptions.api;
+    }
+
+    getGridExtras(){
+        return this.gridExtras;
+    }
+}
+
+export {ExtendedDataGrid};
